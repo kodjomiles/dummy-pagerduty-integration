1d48d6ab77877446195fdda38eb7e4a702281644
Jaden Miles <154226440+jadenmyles@users.noreply.github.com>
2024-08-01 06:20:45 +0000

    - Testing slack PR automation 7

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Testing slack integration PR 6"}
+    return {"message": "Testing slack integration PR 7"}
 
 
 @app.get("/hello/{name}")
